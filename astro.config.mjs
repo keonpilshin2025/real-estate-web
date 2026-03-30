0086b325c5dc2fc1a40b67bf4777680443c249da
bultus <bultus@naver.com>
2026-03-30 20:00:48 +0900

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [
+    sitemap(),
+    icon()
+  ],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
---
+++ postcss.config.mjs
@@ -0,0 +1,6 @@
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {}, // 기존 'tailwindcss' 대신 이걸 사용합니다.
+    'autoprefixer': {},
+  },
+}
